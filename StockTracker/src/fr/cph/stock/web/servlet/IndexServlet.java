12958ff69ed0d84a118cad1cd5420eb86bc3280e
carl <cp.harmant@gmail.com>
2015-04-18 10:49:25 -0500

    Clean code

--- StockTracker/src/fr/cph/stock/web/servlet/IndexServlet.java
+++ StockTracker/src/fr/cph/stock/web/servlet/IndexServlet.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-
 @WebServlet(name = "IndexServlet", urlPatterns = { "/index" })
 public class IndexServlet extends HttpServlet {
 
