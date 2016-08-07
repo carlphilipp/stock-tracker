4237f1c6e2143e35c2f38de110008bcd363da87e
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-08-06 23:41:35 -0500

    Refactor account servlet

--- src/main/java/fr/cph/stock/web/servlet/accounts/AccountsServlet.java
+++ src/main/java/fr/cph/stock/web/servlet/accounts/AccountsServlet.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Carl-Philipp Harmant
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,9 +41,8 @@ import static fr.cph.stock.util.Constants.*;
  * This servlet is called to access the accounts page
  *
  * @author Carl-Philipp Harmant
- *
  */
-@WebServlet(name = "AccountsServlet", urlPatterns = { "/accounts" })
+@WebServlet(name = "AccountsServlet", urlPatterns = {"/accounts"})
 public class AccountsServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -5015939908893417514L;
@@ -63,53 +62,22 @@ public class AccountsServlet extends HttpServlet {
 		try {
 			final HttpSession session = request.getSession(false);
 			final User user = (User) session.getAttribute(USER);
-
 			final String add = request.getParameter(ADD);
 			final String mod = request.getParameter(MOD);
 			final String del = request.getParameter(DELETE);
-			if (add != null) {
-				final String acc = request.getParameter(ACCOUNT);
-				final String currency = request.getParameter(CURRENCY);
-				final String liquidity = request.getParameter(LIQUIDITY);
+			final String lang = CookieManagement.getCookieLanguage(Arrays.asList(request.getCookies()));
 
-				final Account account = new Account();
-				account.setName(acc);
-				account.setCurrency(Currency.getEnum(currency));
-				account.setLiquidity(Double.valueOf(liquidity));
-				account.setUserId(user.getId());
-				account.setDel(true);
-				business.addAccount(account);
-				request.setAttribute(MESSAGE, ADDED);
+			if (add != null) {
+				addAccount(request, user.getId());
 			}
 			if (mod != null) {
-				final String id = request.getParameter(ID);
-				final String acc = request.getParameter(ACCOUNT);
-				final String currency = request.getParameter(CURRENCY);
-				final String liquidity = request.getParameter(LIQUIDITY);
-				final Account account = new Account();
-				account.setName(acc);
-				account.setCurrency(Currency.getEnum(currency));
-				account.setLiquidity(Double.valueOf(liquidity));
-				account.setUserId(user.getId());
-				account.setId(Integer.parseInt(id));
-				business.updateAccount(account);
-				request.setAttribute(MESSAGE, MODIFIED_MESSAGE);
+				modifyAccount(request, user.getId());
 			}
 			if (del != null) {
-				final String id = request.getParameter(ID);
-				final String delete = request.getParameter(DELETE_2);
-				LOG.debug(delete);
-				if (delete.equals("true")) {
-					final Account account = new Account();
-					account.setId(Integer.parseInt(id));
-					business.deleteAccount(account);
-				} else {
-					request.setAttribute(ERROR, "You are not allowed to delete this account!");
-				}
+				deleteAccount(request);
 			}
 			final Portfolio portfolio = business.getUserPortfolio(user.getId(), null, null);
-
-			final String lang = CookieManagement.getCookieLanguage(Arrays.asList(request.getCookies()));
+			
 			request.setAttribute(LANGUAGE, language.getLanguage(lang));
 			request.setAttribute(PORTFOLIO, portfolio);
 			request.setAttribute(CURRENCIES, Currency.values());
@@ -118,6 +86,51 @@ public class AccountsServlet extends HttpServlet {
 		} catch (final Throwable t) {
 			LOG.error(t.getMessage(), t);
 			throw new ServletException("Error: " + t.getMessage(), t);
+		}
+	}
+
+	private void addAccount(final HttpServletRequest request, final int userId) {
+		final String acc = request.getParameter(ACCOUNT);
+		final String currency = request.getParameter(CURRENCY);
+		final String liquidity = request.getParameter(LIQUIDITY);
+
+		final Account account = new Account();
+		account.setName(acc);
+		account.setCurrency(Currency.getEnum(currency));
+		account.setLiquidity(Double.valueOf(liquidity));
+		account.setUserId(userId);
+		account.setDel(true);
+		business.addAccount(account);
+
+		request.setAttribute(MESSAGE, ADDED);
+	}
+
+	private void modifyAccount(final HttpServletRequest request, final int userId) {
+		final String id = request.getParameter(ID);
+		final String acc = request.getParameter(ACCOUNT);
+		final String currency = request.getParameter(CURRENCY);
+		final String liquidity = request.getParameter(LIQUIDITY);
+
+		final Account account = new Account();
+		account.setName(acc);
+		account.setCurrency(Currency.getEnum(currency));
+		account.setLiquidity(Double.valueOf(liquidity));
+		account.setUserId(userId);
+		account.setId(Integer.parseInt(id));
+		business.updateAccount(account);
+
+		request.setAttribute(MESSAGE, MODIFIED_MESSAGE);
+	}
+
+	private void deleteAccount(final HttpServletRequest request) {
+		final String id = request.getParameter(ID);
+		final String delete = request.getParameter(DELETE_2);
+		if (delete.equals(Boolean.toString(true))) {
+			final Account account = new Account();
+			account.setId(Integer.parseInt(id));
+			business.deleteAccount(account);
+		} else {
+			request.setAttribute(ERROR, "You are not allowed to delete this account!");
 		}
 	}
 
