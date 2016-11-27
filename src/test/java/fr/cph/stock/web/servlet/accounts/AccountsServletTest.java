36303b53b63d265c95a243562d4ce563f463d7ed
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-11-26 22:27:06 -0600

    Fix mockito deprecated import

--- src/test/java/fr/cph/stock/web/servlet/accounts/AccountsServletTest.java
+++ src/test/java/fr/cph/stock/web/servlet/accounts/AccountsServletTest.java
@@ -13,7 +13,7 @@ import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
