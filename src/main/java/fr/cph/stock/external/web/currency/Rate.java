0b05ebb510c81b4b7297f0e75f430c63579c02b8
Carl-Philipp Harmant <cp.harmant@gmail.com>
2016-12-03 15:58:35 -0600

    Remove useless attribute in Rate

--- src/main/java/fr/cph/stock/external/web/currency/Rate.java
+++ src/main/java/fr/cph/stock/external/web/currency/Rate.java
@@ -7,21 +7,6 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Rate {
-	private String id;
-	@SerializedName("Name")
-	private String name;
 	@SerializedName("Rate")
 	private double rate;
-	@SerializedName("Date")
-	private String date;
-	@SerializedName("Time")
-	private String time;
-	@SerializedName("Ask")
-	private double ask;
-	@SerializedName("Bid")
-	private double bid;
-
-	public double getRate() {
-		return rate;
-	}
 }
